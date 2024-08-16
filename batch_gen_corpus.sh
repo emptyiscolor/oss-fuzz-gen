9fd36457b81352565d30d173c484cc955debf116
emptyiscolor <emptyiscolor@gmail.com>
2024-08-16 00:26:08 -0500

    update batch scripts

--- batch_gen_corpus.sh
+++ batch_gen_corpus.sh
@@ -2,6 +2,7 @@
 proj_input_file="/tmp/input.txt"
 to_generated_file="/mydata/data/code/fuzzing/oss-fuzz/to_generated.txt"
 to_count_cov_proj_file="/mydata/data/code/fuzzing/oss-fuzz/to_count_cov_proj.txt"
+CSV_HARNESS_FILE="/mydata/data/code/fuzzing/oss-fuzz-gen/filtered_harness.csv"
 BENCHMARK_OSS_SEEDS_DIR="/mydata/data/code/fuzzing/oss-fuzz-gen"
 OSSFUZZ_DIR="/mydata/data/code/fuzzing/oss-fuzz"
 
@@ -49,8 +50,10 @@ function run_batch_seedgen_scripts() {
   for project in $(cat $to_count_cov_proj_file); do
     echo "Running batch seedgen for project: $project"
     pushd "$OSSFUZZ_DIR" && \
-      mkdir -p build/work/$project
-      echo "cp -f $BENCHMARK_OSS_SEEDS_DIR/save_ai_corpus.sh ./build/work/$project/"
+      mkdir -p build/work/$project && \
+      python infra/helper.py build_image --no-pull $project && \
+      # python infra/helper.py build_fuzzers --sanitizer=coverage $project && \
+      echo "cp -f $BENCHMARK_OSS_SEEDS_DIR/save_ai_corpus.sh ./build/work/$project/" && \
       cp -f $BENCHMARK_OSS_SEEDS_DIR/save_ai_corpus.sh ./build/work/$project/ && \
       mkdir -p ./build/work/$project/corpus && \
       cp -f $BENCHMARK_OSS_SEEDS_DIR/benchmark-seedgen/$project/*.py ./build/work/$project/ && \
@@ -59,7 +62,51 @@ function run_batch_seedgen_scripts() {
   done
 }
 
+function copy_generated_corpus() {
+  # Find all aigen_corpus directories under build/work/
+  pushd $OSSFUZZ_DIR
+  find build/work/ -iname aigen_corpus | while read -r corpus_path; do
+      # Extract the project name from the path
+      project=$(basename "$(dirname "$corpus_path")")
+
+      # Create the target directory for the project
+      mkdir -p "build/corpus/$project"
+
+      # Copy the aigen_corpus directory to the target directory
+      cp -r "$corpus_path" "build/corpus/$project/"
+  done
+  popd
+}
+
+function generate_cov() {
+  # python infra/helper.py coverage --fuzz-target=$binary_name --corpus-dir=$corpus_dir $project --no-serve
+  while IFS= read -r line; do
+    # Split the line into fields using ':' as the delimiter
+    IFS=',' read -r -a fields <<< "$line"
+
+    # Extract project_name and source_code_file
+    project_name="${fields[0]}"
+    binary_name="${fields[1]}"
+    source_code_file_with_info="${fields[2]}"
+
+    # Remove the line number and column number from source_code_file
+    source_code_file="${source_code_file_with_info%:*:*}"
+
+    echo "Project Name: $project_name, Binary Name: $binary_name, Source Code File: $source_code_file"
+
+    # if build/cov_report/builtin/$project exists, skip
+    if [ -d "$OSSFUZZ_DIR/build/corpus/$project_name/aigen_corpus" ] ; then
+      echo "Generating code coverage: $project_name"
+      python infra/helper.py build_fuzzers --sanitizer=coverage $project_name
+      python infra/helper.py coverage --fuzz-target=$binary_name --corpus-dir="$OSSFUZZ_DIR/build/corpus/$project_name/aigen_corpus" $project_name --no-serve
+    fi
+
+  done < "$CSV_HARNESS_FILE"
+}
+
 # copy_src_from_docker
-batch_gen_seeds
+# batch_gen_seeds
 
 # run_batch_seedgen_scripts
+
+generate_cov
